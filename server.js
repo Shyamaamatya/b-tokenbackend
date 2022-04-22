9488b64cd7e54fba499316c4b4c7c0a189d72d20
Shyamaamatya <shyama.amatya@asterdio.com>
2022-04-22 23:33:28 +0545

    login stuff

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const userRouter = require("./routes/User")
 const tokenRouter = require("./routes/Token")
 const connectDatabase = require("./db")
 const dotenv = require("dotenv");
+var cors = require('cors');
 // Setting up config file
 dotenv.config({ path: "./config.env" });
 
 connectDatabase()
 
+
+app.use(cors());
 app.use(express.json())
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/token", tokenRouter)
