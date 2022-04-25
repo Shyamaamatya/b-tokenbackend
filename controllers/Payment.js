cb95f5b497aeb2dae4423999caceafb3f81b64af
Shyamaamatya <shyama.amatya@asterdio.com>
2022-04-25 21:13:10 +0545

    payment error

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -9,17 +9,18 @@ let data = {
     "token": req.body.token,
     "amount": req.body.amount
 };
-console.log(data)
+// console.log("data", data)
 
 
 let config = {
-    headers: {'Authorization': 'Key test_secret_key_f59e8b7d18b4499ca40f68195a846e9b'}
+    headers: {'Authorization': 'Key test_secret_key_03d8c5e11949469f80e385d0d1a50e1d'}
 };
 
 axios.post("https://khalti.com/api/v2/payment/verify/", data, config)
     .then(response => {
       res.status(200).json({status:"Success",mesage:"Payment Success",response})    })
     .catch(error => {
+        console.log(error.message)
         res.status(400).json({"status":"Failure",error})
     });
   
